439f1e67780e6c40062013a43e3ab89b72562d3e
Ottriman <gogo.gogo520@gmail.com>
2016-05-03 11:25:18 +0000

    DatabaseTest_Commit

---
+++ verknam2hopur_42_43/DatabaseTest/DatabaseTest/Controllers/AssignmentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseTest.Services;
+
+namespace DatabaseTest.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private AssignmentService _service = new AssignmentService();
+        
+        // GET: Assignments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var viewModel = _service.GetAssignmentByID(id);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ verknam2hopur_42_43/DatabaseTest/DatabaseTest/Models/Entities/AssignmentMilestone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseTest.Models.Entities
+{
+    // A milestone in an assignment is worth some part of the assignment
+    // Each milestone thus has a weight representing its importance
+    //Assigment ID is a key to the assignment
+
+    public class AssignmentMilestone
+    {
+        public int ID { get; set; }
+        public int AssignmentID { get; set; }
+        public string Title { get; set; }
+        public int Weight { get; set; }
+    }
+}
