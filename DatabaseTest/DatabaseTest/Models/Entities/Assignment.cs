70c746022a70f7ff6253e775711bd75791713be4
druma <hilara_roma_esaran_necture@hotmail.com>
2016-05-06 14:12:37 +0000

    Revert "Merge branch 'master' of https://github.com/Druma/verknam2hopur_42_43"
    This reverts commit fe2e2d960c915ae45cf367c4ce9219e55cc784d9, reversing
    changes made to eeb5a643491cf34fa471600a96ea97747836f3b0.

--- DatabaseTest/DatabaseTest/Models/Entities/Assignment.cs
+++ DatabaseTest/DatabaseTest/Models/Entities/Assignment.cs
@@ -1,38 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 namespace DatabaseTest.Models.Entities
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
-    public partial class assignment
+    public class Assignment
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public assignment()
-        {
-            groups = new HashSet<group>();
-        }
-
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
-
-        [Required]
-        public string name { get; set; }
-
-        public int? maxSubmisions { get; set; }
-
-        public int? groupSize { get; set; }
-
-        public DateTime startDate { get; set; }
-
-        public DateTime endDate { get; set; }
-
-        public int courseID { get; set; }
-
-        public virtual cours cours { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<group> groups { get; set; }
+        public int CourseID { get; set; }
+        public string Title { get; set; }
     }
 }
--- DatabaseTest/DatabaseTest/Models/Entities/Course.cs
+++ DatabaseTest/DatabaseTest/Models/Entities/Course.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace DatabaseTest.Models.ViewModels
+namespace DatabaseTest.Models.Entities
 {
-    public class UsersViewModel
+    public class Course
     {
+        public int ID { get; set;}
         public string Name { get; set; }
     }
 }
