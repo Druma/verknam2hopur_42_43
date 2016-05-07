fc24377b55f0d77de9ae9d3d14ade157e6ba75c3
EgillM <egillh14@ru.is>
2016-05-07 18:52:28 +0000

    smá í admin

---
+++ Mooshak2/Mooshak2/ViewModels/AdminViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.ViewModels
+{
+    public class AdminViewModel
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public virtual userType UserType { get; set; }
+        
+    }
+}
