d492c6e2637d62510e2faaf1e0aacfb7fbdd3ad6
EgillM <egillh14@ru.is>
2016-05-10 10:18:20 +0000

    tab functionality in admin

---
+++ Mooshak2/Mooshak2/Services/CoursesService.cs
@@ -0,0 +1,27 @@
+﻿using Mooshak2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Services
+{
+    public class CoursesService
+    {
+        private dbContext _db;
+
+        public CoursesService()
+        {
+            _db = new dbContext();
+        }
+        public List<CoursesViewModel> getAllCourses()
+        {
+            var result = _db.courses.Select(x => new CoursesViewModel
+            {
+                Name = x.name
+            }).ToList();
+
+            return result;
+        }
+    }
+}
---
+++ Mooshak2/Mooshak2/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
